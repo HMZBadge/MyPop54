c52e8aa9a5b1c1fd7c8ba3984a8e3490a280a3fd
mybadge <zhaozhidan@iqianbang.com>
2018-05-04 18:30:07 +0800

    add some

--- js/Common/RepositoryCell.js
+++ js/Common/RepositoryCell.js
@@ -1,15 +1,41 @@
 import React from 'react';
 
-import { 
+import {
     View,
     StyleSheet,
     Text,
     TouchableOpacity,
     Image
- } from 'react-native';
+} from 'react-native';
 
- export default class RepositoryCell extends Comment {
-     constructor(props) {
-         super(props);
+export default class RepositoryCell extends Comment {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isFavorite: this.props.projectModel.isFavorite,
+            favoriteIcon: RepositoryCell.getIcon(this.props.projectModel.isFavorite),
+        };
+    }
+
+    static getIcon(isFavorite) {
+        return isFavorite ? require('../../res/images/ic_star.png') : require('../../res/images/ic_unstar_transparent.png');
+    }
+
+    render() {
+        let item = this.props.projectModel.item ? this.props.projectModel.item : this.props.projectModel;
+        // let favoriteButton = this.props.projectModel.item ?
+        //     <TouchableOpacity
+        //         style={{ padding: 6 }}
+        //         onPress={() => this.onPressFavorite()}
+        //         underlayColor='transparent'>
+        //         <Image 
+        //             style={{padding: 6}}
+        //             // onPress=
+        //             // />
+
+
+        //     </TouchableOpacity> : null;
+
+        return nil;
      }
- }
+}
