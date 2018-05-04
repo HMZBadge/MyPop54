39fd5d201f19ff3073fccdd42091e93efefd939c
mybadge <zhaozhidan@iqianbang.com>
2018-05-04 15:59:17 +0800

    add some

---
+++ js/Common/RepositoryCell.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { 
+    View,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    Image
+ } from 'react-native';
+
+ export default class RepositoryCell extends Comment {
+     constructor(props) {
+         super(props);
+     }
+ }
